c80bfb06c045f093d215ac41baa739c6ba65fb3a
muszynski <muszynski@gmail.com>
2023-03-05 16:27:18 +0100

    struct

---
+++ alkoFree/alkoFree/Data/dataModelStruct.swift
@@ -0,0 +1,40 @@
+//
+//  dataModelStruct.swift
+//  alkoFree
+//
+//  Created by Łukasz Muszyński on 05/03/2023.
+//
+
+import Foundation
+
+//Struktura "User":
+
+struct User{
+    var gender : String
+    var weight : Double
+    var height : Double
+}
+//Struktura "Drink":
+
+struct Drink {
+    var name: String
+    var alcoholConcentration: Double
+    var volume: Double
+    var otherParameters: [String: Any] // słownik do przechowywania innych parametrów
+}
+
+// Struktura Measurement
+struct Measurement {
+    var user: User
+    var drink: Drink
+    var measurementTime: Date
+    var result: Double
+    var stomachState: String
+}
+
+//Struktura "Database":
+struct Database{
+    var user : [User]
+    var drink : [Drink]
+    var measurement : [Measurement]
+}
