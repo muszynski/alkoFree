0f1c2819711fdb522ef41a0d26e578d06149d5a1
muszynski <muszynski@gmail.com>
2023-03-05 16:05:32 +0100

    init

---
+++ alkoFree/alkoFree/alkoFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  alkoFreeApp.swift
+//  alkoFree
+//
+//  Created by Łukasz Muszyński on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct alkoFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
